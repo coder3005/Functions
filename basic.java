5fda085e4ca2839c9bac33abadc63d4c8a7f88ff
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-01-25 23:52:16 +0530

    Create basic.java

---
+++ basic.java
@@ -0,0 +1,13 @@
+package Functions;
+
+public class basic {
+    public static void printHelloWorld() {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        return; // we also can't write return in case of 'void'
+    }
+    public static void main(String args[]) {
+        printHelloWorld(); //function call
+    }
+}
